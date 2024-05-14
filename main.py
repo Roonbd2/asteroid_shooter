013083d3a5d85a9e9345534b2c95489d9f776c6b
Dean Rooney <165762801+Roonbd2@users.noreply.github.com>
2024-05-14 10:37:28 +1000

    Laser timer implemented

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ def laser_update(laser_list, speed = 300):
         if rect.bottom < 0:
             laser_list.remove(rect)
 
+# function to display the score as time since pygame.init was called
+def display_score():
+    score_text = f"Score: {pygame.time.get_ticks() // 1000}"
+    text_surf = font.render(score_text, True, "White")
+    text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, 'white', text_rect.inflate(30, 10), width = 2, border_radius = 5)
+
+# function that checks if we are able to shoot yet
+def laser_timer(can_shoot, duration = 500):
+    if not can_shoot:
+        current_time = pygame.time.get_ticks()
+        if current_time - shoot_time > duration:
+            can_shoot = True
+    return can_shoot
+
+
 # game init
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -22,13 +39,11 @@ background = pygame.image.load('graphics/background.png').convert()
 # laser import
 laser_surf = pygame.image.load('graphics/laser.png').convert_alpha()
 laser_list = []
-#laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
+#laser timer
+can_shoot = True
+shoot_time = None
 # text import
-font = pygame.font.Font('graphics/subatomic.ttf', 50) 
-text_surf = font.render('Space', True, "White")
-text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
-
-test_rect = pygame.Rect(100,200,400,500)
+font = pygame.font.Font('graphics/subatomic.ttf', 50)
 
 #Keeps the code going
 while True: #Run forever -> Keeps the game running
@@ -37,9 +52,13 @@ while True: #Run forever -> Keeps the game running
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and can_shoot:
+            #laser
             laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
             laser_list.append(laser_rect)
+            #timer logic
+            can_shoot = False
+            shoot_time = pygame.time.get_ticks()
       
     # framerate limit
     dt = clock.tick(120) / 1000
@@ -49,14 +68,14 @@ while True: #Run forever -> Keeps the game running
 
     # update
     laser_update(laser_list)
+    can_shoot = laser_timer(can_shoot, 500)
 
     # drawing
     display_surface.fill((0,0,0))
     display_surface.blit(background, (0,0))
 
-    display_surface.blit(text_surf, text_rect)
-    pygame.draw.rect(display_surface, 'white', text_rect.inflate(30, 10), width = 2, border_radius = 5)
-    
+    display_score()
+
     # for loop that draws the laser surface where the rects are
     for rect in laser_list:
         display_surface.blit(laser_surf, rect)
